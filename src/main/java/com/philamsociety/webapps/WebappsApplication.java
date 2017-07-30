ff962aa9b4a47e2b422e1704c5abb8c7b71b1d5b
Chris Francisco <ChrisF@Riyel-iMac.local>
2017-07-30 13:37:39 -0400

    hello

---
+++ src/main/java/com/philamsociety/webapps/WebappsApplication.java
@@ -0,0 +1,14 @@
+package com.philamsociety.webapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class WebappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappsApplication.class, args);
+	}
+}
